94593d9b6667d9d870d691bd486dcd2aaae95072
Luke Sheppard <sheppiegit@gmail.com>
2023-07-16 17:38:17 -0400

    Added some logic to play around with.

--- src/BasicProxy.sol
+++ src/BasicProxy.sol
@@ -1,8 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-contract BasicProxy {}
+//EOA -> Proxy -> Logic1
+//            -> Can change to logic2
 
-contract Logic1 {}
+contract BasicProxy {
+    address implementation;
+
+    function changeImplementation(address _implementation) external {
+        implementation = _implementation;
+    }
+
+    function changeX(uint256 _x) public {
+        Logic1(implementation).changeX(_x);
+    }
+}
+
+contract Logic1 {
+    uint256 public x = 0;
+
+    function changeX(uint256 _x) public {
+        x = _x;
+    }
+}
 
 contract Logic2 {}
