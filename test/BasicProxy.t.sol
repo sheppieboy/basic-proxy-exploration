319e41ce4bdcf596b9a109295a059f76701ad014
Luke Sheppard <sheppiegit@gmail.com>
2023-07-16 18:17:49 -0400

    test to check that proxy can manipulate both sets of logic contracts.

--- test/BasicProxy.t.sol
+++ test/BasicProxy.t.sol
@@ -16,10 +16,23 @@ contract BasicProxyTest is Test {
         proxy = new BasicProxy();
     }
 
-    function test_changeX() public {
+    function test_changeXInLogic1() public {
         proxy.changeImplementation(address(logic1));
         proxy.changeX(52);
         uint256 x = logic1.x();
         assertEq(x, 52);
+    }
+
+    function test_changeXInLogic1And2() public {
+        proxy.changeImplementation(address(logic1));
+        proxy.changeX(52);
+        uint256 x1 = logic1.x();
+
+        proxy.changeImplementation(address(logic2));
+        proxy.changeX(45);
+        uint256 x2 = logic2.x();
+
+        assertEq(x1, 52);
+        assertEq(x2, 45);
     }
 }
