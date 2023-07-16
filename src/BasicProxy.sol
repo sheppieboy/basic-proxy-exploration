5b13bc04d6c76ed482be4a694ea7a741fddd0e20
Luke Sheppard <sheppiegit@gmail.com>
2023-07-16 17:28:30 -0400

    initial project testing and contract setup.

---
+++ src/BasicProxy.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract BasicProxy {}
+
+contract Logic1 {}
+
+contract Logic2 {}
---
+++ test/BasicProxy.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.17;
+
+import "../src/BasicProxy.sol";
+import "forge-std/Test.sol";
+
+contract BasicProxyTest is Test {
+    function setUp() public {}
+}
